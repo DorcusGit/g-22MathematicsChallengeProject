1cd7b9545890e5b782ffc48b75ef127f01d86750
DorcusGit <dorcusarigye9@gmail.com>
2024-07-11 21:00:55 +0300

    Established a connection between the client and the server class through the socket

--- Client.java
+++ Client.java
@@ -1,6 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client{
 
-    public static void main(String [] args){
+    public static void main(String [] d){
+         
+        try{
+    
+        Socket soc=new Socket("localhost", 5121);
+        BufferedReader B=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter P=new PrintWriter(soc.getOutputStream(),true);
+        BufferedReader Br=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        System.out.println("\t Welcome to mathematics Challenge System ");
 
+    }catch(IOException e){
+        e.printStackTrace();
     }
+}
 }
--- Server.java
+++ Server.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server{
     public static void main(String [] args){
+        try{
+            ServerSocket ss=new ServerSocket(5121);
+              
+              System.out.println("\tMathematics Challenge System. waiting for the client.....");
+              Socket soc=ss.accept();
+        }catch(IOException e){
+            e.printStackTrace();
+
+        }
         
     }
 }
